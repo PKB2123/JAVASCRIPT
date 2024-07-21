c0680ec1f6e3fb57b5cef79b7409d07483c6aac6
Prabal Kanti Bhunia <151463185+PKB2123@users.noreply.github.com>
2024-07-21 10:43:58 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as never version
+
+
+let name = "Prabal"
+let age = 18
+let isLoggedIn = false
+
+
+// number=> 2 to power 53
+// bigint for big size number
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof "Prabal"); //string
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
