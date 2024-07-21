c2ee411d92c4cccbfc556b74cb3bf043aefcc9fe
Prabal Kanti Bhunia <151463185+PKB2123@users.noreply.github.com>
2024-07-21 09:04:12 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId=7407
+let accountEmail="pkb@gmail.com"
+let accountPassword="212345"
+
+
+accountEmail="prabal@gmail.com"
+
+accountCity="Kolkata"
+accountName="Prabal Kanti Bhunia"
+accountPh_no="7407253074"
+
+console.table([accountName, accountId, accountEmail, accountPassword, accountCity, accountPh_no])
