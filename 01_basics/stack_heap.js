36e530c83b28a8145a9ace80f31e993357bbd622
Prabal Kanti Bhunia <151463185+PKB2123@users.noreply.github.com>
2024-07-22 13:53:48 +0000

    meomory in js

---
+++ 01_basics/stack_heap.js
@@ -0,0 +1,21 @@
+//stack(Premitive), heap(Non-premitive)
+
+let myName="prabal@google.com"
+let anotherName=myName
+anotherName="bhunia@gmail.com"
+console.log(myName);
+console.log(anotherName);
+
+
+
+
+let userOne={
+    Email:"prabalkanti@gmail.com",
+    Pw:2123
+}
+
+let userTwo=userOne
+
+userTwo.Email="pkb@gmail.com"
+console.log(userOne.Email);
+console.log(userTwo.Email);
